839a1ea1f1cba1cbaa94ccea7c684d89c45861fc
Burhan Öztürk <burh0125@edu.easj.dk>
2018-11-01 21:05:59 +0100

    PrintLN

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -4,6 +4,7 @@ public class AccountTest
 {
     public static void main(String[] args)
     {
+        //New object
         Account accountone=new Account(1122,20000);
         accountone.setAnnualInterestRate(4.5);
         accountone.withdraw(2500);
